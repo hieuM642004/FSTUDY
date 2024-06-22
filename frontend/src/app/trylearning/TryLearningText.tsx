ae8aa5fc0088c6916459a652d9378da351f88094
duynhpc05141 <duynhpc05141@fpt.edu.vn>
2024-06-22 14:42:06 +0700

    [FE]-[UI]-[FP-5]-Try Learning

---
+++ frontend/src/app/trylearning/TryLearningText.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React, { useState } from 'react';
+import {
+    LeftOutlined,
+    RightOutlined,
+    BookOutlined,
+    MenuOutlined,
+} from '@ant-design/icons';
+import { MenuProps } from 'antd';
+import { Button, Menu, Drawer } from 'antd';
+import './TryLearning.scss'; // Import CSS for custom styling
+import Link from 'next/link';
+
+const TryLearningText: React.FC = () => {
+    return <>hello</>;
+};
+
+export default TryLearningText;
