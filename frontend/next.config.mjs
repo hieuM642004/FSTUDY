96cd0f78dcad586072406ada7c175efeb96e8edb
loikt <loiktpc05314@fpt.edu.vn>
2024-07-11 19:21:25 +0700

    [FE]-[FIX]-[FP-19]-Blogs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['tiki.vn' , 'glotcms.sgp1.cdn.digitaloceanspaces.com' , 'study4.com'],
+    domains: ['tiki.vn' , 'glotcms.sgp1.cdn.digitaloceanspaces.com' , 'study4.com' ,'scontent.fsgn5-10.fna.fbcdn.net'],
   },
 };
 
