b57c94c8d1f45215d5eebd6388a9ace17fce2cb2
hieuM642004 <136356604+hieuM642004@users.noreply.github.com>
2024-09-10 13:58:18 +0700

    [BE]-[REFACTOR]-Modify in course controller

--- backend/nest/src/apis/Course/course.controller.ts
+++ backend/nest/src/apis/Course/course.controller.ts
@@ -395,7 +395,7 @@ export class CourseController {
     
 
     @Get(':id')
-    findOneCourse(@Param('slug') id: string): Promise<Course> {
+    findOneCourse(@Param('id') id: string): Promise<Course> {
         return this.courseService.findOneCourse(id);
     }
     @Get('search/:slug')
